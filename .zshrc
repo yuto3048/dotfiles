1c464bf3cfcff4f04afda6e3540bbcfaaffd45ca
yuto3048 <yuto101024@gmail.com>
2018-04-20 10:18:57 +0900

    new file:   .xmobarrc new file:   .xmobarrc_bak modified:   .zshrc modified:   dein/plugins.toml modified:   xmonad/xmonad.hs

--- .zshrc
+++ .zshrc
@@ -8,6 +8,8 @@ export LANG=ja_JP.UTF-8  # 文字コードをUTF-8に設定
 export KCODE=u           # KCODEにUTF-8を設定
 export AUTOFEATURE=true  # autotestでfeatureを動かす
 
+setxkbmap -layout jp
+
 echo -ne "\033]0;${USER}@${HOST%%.*}\007"
 
 
--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -2,17 +2,38 @@ import XMonad
 import XMonad.Config.Desktop
 import XMonad.Actions.CycleWS
 import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.SetWMName
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run
 
 main = do
+  myStatusBar <- spawnPipe "xmobar ~/.xmobarrc_new"
   xmonad $ defaultConfig
     { terminal = myTerminal
     , modMask = myModMask
     , workspaces = myWorkspaces
+    , startupHook = myStartupHook
+    , layoutHook = myLayoutHook
+    , manageHook = myManageHook
+    , logHook = myLogHook myStatusBar
+--    , handleEventHook = fullscreenEventHook
+--    , startupHook = setWMName "LG3D"
     } `additionalKeys` myAdditionalKeys
 
 myTerminal = "terminator"
 myModMask = mod4Mask
-myWorkspaces = ["1","2","3","4"]
+myWorkspaces = ["1","2","3","4","5","6","7","8","9"]
+
+myStartupHook = do
+    spawnOnce = "nm-applet"
+
+myLayoutHook = avoidStruts $ layoutHook defaultConfig
+myManageHook = manageDocks <+> manageHook defaultConfig
+myLogHook h = dynamicLogWithPP xmobarPP 
+    { ppOutput = hPutStrLn h
+    }
 
 myAdditionalKeys = 
     [ ((mod1Mask .|. controlMask, xK_Right), nextWS)
